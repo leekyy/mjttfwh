1504ecc814ca0b6094e1ed8af7bdfaf06d2eb121
TerryQi <15840345959@139.com>
2018-03-30 05:49:40 +0800

    优化关注

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -40,29 +40,32 @@ class WechatController extends Controller
                                 return "微信：3011740452";
                         }
                     }
+                    if ($message['Event'] == 'subscribe') {     //关注事件
+                        $text = "hey，又多了一个粉！\r\n接下来的旅行时光里，美景听听会为你带来最优质的景点讲解服务哦~\r\n\r\n<a href=\"http://www.baidu.com\">点击此处</a>，可以申请美景听听幸运用户\r\n点击“美景”可以看到历史主题原创漫画\r\n点击“听听”可以通过喜马拉雅和小程序听景点讲解\r\n点击“App”可以下载美景听听中文语音导游\r\n\r\n有任何疑问，欢迎随时骚扰Tommy微信：3011740452";
+                        return $text;
+                    }
                     break;
                 case 'text':
-                    $text = "hey，又多了一个粉！\r\n接下来的旅行时光里，美景听听会为你带来最优质的景点讲解服务哦~\r\n\r\n<a href=\"http://www.baidu.com\">点击此处</a>，可以申请美景听听幸运用户\r\n点击“美景”可以看到历史主题原创漫画\r\n点击“听听”可以通过喜马拉雅和小程序听景点讲解\r\n点击“App”可以下载美景听听中文语音导游\r\n\r\n有任何疑问，欢迎随时骚扰Tommy微信：3011740452";
-                    return $text;
+
                     break;
                 case 'image':
-                    return '收到图片消息';
+                    return '';
                     break;
                 case 'voice':
-                    return '收到语音消息';
+                    return '';
                     break;
                 case 'video':
-                    return '收到视频消息';
+                    return '';
                     break;
                 case 'location':
-                    return '收到坐标消息';
+                    return '';
                     break;
                 case 'link':
-                    return '收到链接消息';
+                    return '';
                     break;
                 // ... 其它消息
                 default:
-                    return '收到其它消息';
+                    return '';
                     break;
             }
         });
