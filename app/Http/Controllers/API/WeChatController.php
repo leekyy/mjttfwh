0d2a52c2503fa08eaa24ba58b6128177b57e37df
TerryQi <15840345959@139.com>
2018-04-02 16:30:15 +0800

    11

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -119,7 +119,7 @@ class WechatController extends Controller
                     //邀请码测试
                     if ($message['Content'] == '群发消息') {
                         $app = app('wechat.official_account');
-                        $app->broadcasting->sendText("你的好友TerryQi帮你扫码了，还差1个好友助力，即可获得邀请码", ['oJpZ11DU7GZpoW9W_NB5HwXrlYd8', 'oJpZ11Kp7haPT4p0aLXVrF2LgCHY']);
+                        $app->broadcasting->sendText("你的好友TerryQi帮你扫码了，还差6个好友助力，即可获得邀请码", ['oJpZ11DU7GZpoW9W_NB5HwXrlYd8', 'oJpZ11Kp7haPT4p0aLXVrF2LgCHY']);
                     }
 
                     break;
