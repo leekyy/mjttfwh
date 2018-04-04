e94cf814e7cca416d9ef914f8e513c0ea9bfd92a
TerryQi <15840345959@139.com>
2018-04-04 16:02:02 +0800

    优化

--- app/Http/Controllers/Html5/LuckUserController.php
+++ app/Http/Controllers/Html5/LuckUserController.php
@@ -27,7 +27,7 @@ class LuckUserController
     public function index(Request $request)
     {
         $user = session('wechat.oauth_user'); // 拿到授权用户资料
-
+        dd($user);
 
         return view('html5.activity.luckUser', ['user' => $user]);
     }
