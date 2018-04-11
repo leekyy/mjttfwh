7dcb9e06fa5d15bcd0257ac0863e8af9ed37e2c3
TerryQi <15840345959@139.com>
2018-04-11 19:55:49 +0800

    111

--- resources/views/html5/activity/luckUser.blade.php
+++ resources/views/html5/activity/luckUser.blade.php
@@ -212,16 +212,16 @@
 
     //点击78元立柯购买
     function click_buy_now() {
-        {{--wx.chooseWXPay({--}}
-        {{--timestamp: '{{$wxPay['timeStamp']}}', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符--}}
-        {{--nonceStr: '{{$wxPay['nonceStr']}}', // 支付签名随机串，不长于 32 位--}}
-        {{--package: '{{$wxPay['package']}}', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）--}}
-        {{--signType: '{{$wxPay['signType']}}', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'--}}
-        {{--paySign: '{{$wxPay['paySign']}}', // 支付签名// 支付成功后的回调函数--}}
-        {{--success: function (res) {--}}
-        {{--alert(JSON.stringify(res));--}}
-        {{--}--}}
-        {{--});--}}
+        wx.chooseWXPay({
+            timestamp: '{{$wxPay['timeStamp']}}', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+            nonceStr: '{{$wxPay['nonceStr']}}', // 支付签名随机串，不长于 32 位
+            package: '{{$wxPay['package']}}', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+            signType: '{{$wxPay['signType']}}', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+            paySign: '{{$wxPay['paySign']}}', // 支付签名// 支付成功后的回调函数
+            success: function (res) {
+                alert(JSON.stringify(res));
+            }
+        });
     }
 
 </script>
