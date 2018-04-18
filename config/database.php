4b2e8a4922a18ab66ab76ef33d63326416060c4e
TerryQi <15840345959@139.com>
2018-04-19 00:22:22 +0800

    111

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '182.92.235.154'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mjttfwhdb'),
-            'username' => env('DB_USERNAME', 'mjttfwh'),
-            'password' => env('DB_PASSWORD', '0WsfHVhH'),
+            'host' => env('DB_HOST', ''),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
